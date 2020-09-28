76e111c64db50d29dbbe8745b6b42b77e717cfde
Nick Chen <nickchenchj@gmail.com>
2020-09-29 01:05:32 +0800

    New features: customary name of output file
    The third argument for customary name of output file is available
    now!

--- int32_generator.c
+++ int32_generator.c
@@ -5,25 +5,36 @@
 
 int main(int argc, char **argv) {
     srand(time(NULL));
-    long long int n = 3e9;  /* Default value */
 
-    if (argc > 2) {
-        puts("Error: Too many arguments");
-        printf("Usage: ./(program_name) [the-number-of-random-integers] (default: %g)\n", (double)n);
-        exit(EXIT_FAILURE);
-    }
- 
-    if (argc == 2)
-        sscanf(argv[1], "%lld", &n);
-    
+    /* Default values */
+    long long int n = 3e9;
     char filename[] = "input.txt";
 
-    /* 
-     * The return value of access() is 0 if the access is permitted, and -1 otherwise.
-     * Ref: https://www.gnu.org/software/libc/manual/html_node/Testing-File-Access.html
+    /* Too many arguments */
+    if (argc > 3) {
+        puts("Error: Too many arguments");
+        printf("Usage: ./(program_name) [the-number-of-random-integers] "
+               "[name-of-output-file]\n"
+               "(default number: %g, default name: %s)\n",
+               (double)n, filename);
+        exit(EXIT_FAILURE);
+    }
+
+    /* Updates default values */
+    if (argc > 1) {
+        sscanf(argv[1], "%lld", &n);
+        if (argc == 3)
+            sscanf(argv[2], "%s", filename);
+    }
+
+    /*
+     * The return value of access() is 0 if the access is permitted, and -1
+     * otherwise.
+     * Ref:
+     * https://www.gnu.org/software/libc/manual/html_node/Testing-File-Access.html
      */
     if (access(filename, F_OK) == 0) {
-        puts("Warning: input.txt already exists");
+        printf("Warning: %s already exists\n", filename);
         puts("Continue? (y: overwrite, n: abort)");
         char c;
         scanf("%c", &c);
@@ -31,12 +42,21 @@ int main(int argc, char **argv) {
             exit(EXIT_FAILURE);
     }
 
-    FILE *fp = fopen(filename, "w");;
+    FILE *fp = fopen(filename, "w");
     int rand_num, sign;
     clock_t start, end;
 
-    puts("Generating random integers...");
+    if (!fp) {
+        printf("Error: Cannot open file %s\n", filename);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("%s created\n"
+           "Generating %lld random integers...\n",
+           filename, n);
     start = clock();
+
+    /* Generates random integers */
     for (long long int i = 0; i < n; i++) {
         rand_num = rand();
         sign = rand() << 31;
@@ -44,6 +64,7 @@ int main(int argc, char **argv) {
         fprintf(fp, "%d\n", rand_num);
     }
     end = clock();
+
     puts("Done!\n---");
     printf("Elapsed Time: %lf secs\n", (double)(end - start) / CLOCKS_PER_SEC);
 
